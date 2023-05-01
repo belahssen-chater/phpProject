88763f5baffc63fc198fb62623ed153bda53eaa8
CHATER <belahssen.chater@itbs.tn>
2023-05-01 23:54:30 +0200

    Product table displayed successfully

--- models/Produit.php
+++ models/Produit.php
@@ -1,66 +1,27 @@
 <?php
-require_once("../Database.php");
+require_once(__DIR__ .  "/../Database.php");
 
-    class Produit{
+    class Produit extends Modele{ 
             
             private $id;
             private $nom;
             private $description;
             private $prix;
-            private $image;
-            private $categorie;
     
-            public function __construct($id = null, $nom = null, $description = null, $prix = null, $image = null, $categorie = null){
+            public function __construct($id = null, $nom = null, $description = null, $prix = null){
+                parent::__construct();
                 $this->id = $id;
                 $this->nom = $nom;
                 $this->description = $description;
                 $this->prix = $prix;
-                $this->image = $image;
-                $this->categorie = $categorie;
+
+            }
+            public function getAll(){
+                $sql = "SELECT * FROM produit";
+                $stmt = $this->pdo->prepare($sql);
+                $stmt->execute();
+                $produits = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                return $produits;
             }
     
-            public function create($data){
-                $db = new Database();
-                $db->query("INSERT INTO produits (nom, description, prix, image, categorie) VALUES (:nom, :description, :prix, :image, :categorie)");
-                $db->bind(':nom', $data['nom']);
-                $db->bind(':description', $data['description']);
-                $db->bind(':prix', $data['prix']);
-                $db->bind(':image', $data['image']);
-                $db->bind(':categorie', $data['categorie']);
-                $db->execute();
-            }
-    
-            public function read(){
-                $db = new Database();
-                $db->query("SELECT * FROM produits");
-                $result = $db->resultSet();
-                return $result;
-            }
-    
-            public function readById($id){
-                $db = new Database();
-                $db->query("SELECT * FROM produits WHERE id = :id");
-                $db->bind(':id', $id);
-                $result = $db->single();
-                return $result;
-            }
-    
-            public function update($data){
-                $db = new Database();
-                $db->query("UPDATE produits SET nom = :nom, description = :description, prix = :prix, image = :image, categorie = :categorie WHERE id = :id");
-                $db->bind(':id', $data['id']);
-                $db->bind(':nom', $data['nom']);
-                $db->bind(':description', $data['description']);
-                $db->bind(':prix', $data['prix']);
-                $db->bind(':image', $data['image']);
-                $db->bind(':categorie', $data['categorie']);
-                $db->execute();
-            }
-    
-            public function delete($id){
-                $db = new Database();
-                $db->query("DELETE FROM produits WHERE id = :id");
-                $db->bind(':id', $id);
-                $db->execute();
-            }
     }
---
+++ views/admin/products.php
@@ -0,0 +1,367 @@
+<?php
+    require_once("../../models/Produit.php");
+    $produit = new Produit();
+
+    $products = $produit->getAll();
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Tables / Data - NiceAdmin Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="../public/assets/img/logo22.png" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../public/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../public/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="../public/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../public/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="../public/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="../public/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../public/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="../public/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: Mar 09 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
+
+    <div  
+
+      href="index.html" class="logo d-flex align-items-center">
+        <img src="../public/assets/img/logo22.png" alt="">
+        <span>STE CHATEUR</span>
+  
+    </div><!-- End Logo -->
+
+
+    <nav class="header-nav ms-auto">
+      <ul class="d-flex align-items-center">
+
+        
+
+        <li class="nav-item dropdown">
+
+          <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+            <i class="bi bi-bell"></i>
+            <span class="badge bg-primary badge-number">4</span>
+          </a><!-- End Notification Icon -->
+
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
+            <li class="dropdown-header">
+              You have 4 new notifications
+              <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li class="notification-item">
+              <i class="bi bi-exclamation-circle text-warning"></i>
+              <div>
+                <h4>Lorem Ipsum</h4>
+                <p>Quae dolorem earum veritatis oditseno</p>
+                <p>30 min. ago</p>
+              </div>
+            </li>
+
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li class="notification-item">
+              <i class="bi bi-x-circle text-danger"></i>
+              <div>
+                <h4>Atque rerum nesciunt</h4>
+                <p>Quae dolorem earum veritatis oditseno</p>
+                <p>1 hr. ago</p>
+              </div>
+            </li>
+
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li class="notification-item">
+              <i class="bi bi-check-circle text-success"></i>
+              <div>
+                <h4>Sit rerum fuga</h4>
+                <p>Quae dolorem earum veritatis oditseno</p>
+                <p>2 hrs. ago</p>
+              </div>
+            </li>
+
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li class="notification-item">
+              <i class="bi bi-info-circle text-primary"></i>
+              <div>
+                <h4>Dicta reprehenderit</h4>
+                <p>Quae dolorem earum veritatis oditseno</p>
+                <p>4 hrs. ago</p>
+              </div>
+            </li>
+
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li class="dropdown-footer">
+              <a href="#">Show all notifications</a>
+            </li>
+
+          </ul><!-- End Notification Dropdown Items -->
+
+        </li><!-- End Notification Nav -->
+
+        <li class="nav-item dropdown">
+
+          <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+            <i class="bi bi-chat-left-text"></i>
+            <span class="badge bg-success badge-number">3</span>
+          </a><!-- End Messages Icon -->
+<!-- End Messages Dropdown Items -->
+
+        </li><!-- End Messages Nav -->
+
+        <li class="nav-item dropdown pe-3">
+
+          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+            <img src="../public/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+            <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+          </a><!-- End Profile Iamge Icon -->
+
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+            <li class="dropdown-header">
+              <h6>Kevin Anderson</h6>
+              <span>Web Designer</span>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                <i class="bi bi-person"></i>
+                <span>My Profile</span>
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                <i class="bi bi-gear"></i>
+                <span>Account Settings</span>
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
+                <i class="bi bi-question-circle"></i>
+                <span>Need Help?</span>
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="#">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Sign Out</span>
+              </a>
+            </li>
+
+          </ul><!-- End Profile Dropdown Items -->
+        </li><!-- End Profile Nav -->
+
+      </ul>
+    </nav><!-- End Icons Navigation -->
+
+  </header><!-- End Header -->
+
+  <!-- ======= Sidebar ======= -->
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="index.html">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li><!-- End Dashboard Nav -->
+
+     
+
+      <li class="nav-item">
+        <a class="nav-link " data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-layout-text-window-reverse"></i><span>Tables</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="tables-nav" class="nav-content collapse show" data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="tables-general.html">
+              <i class="bi bi-circle"></i><span>General Tables</span>
+            </a>
+          </li>
+          <li>
+            <a href="tables-data.html" class="active">
+              <i class="bi bi-circle"></i><span>Data Tables</span>
+            </a>
+          </li>
+        </ul>
+      </li><!-- End Tables Nav -->
+
+      
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="pages-contact.html">
+          <i class="bi bi-envelope"></i>
+          <span>Contact</span>
+        </a>
+      </li><!-- End Contact Page Nav -->
+
+      
+
+    </ul>
+
+  </aside><!-- End Sidebar-->
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Products</h1>
+      
+    </div><!-- End Page Title -->
+
+    <section class="section">
+      <div class="row">
+        <div class="col-lg-12">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Products list</h5>
+              <p>
+                To add, update or delete a product, you'll see 3 buttons bellow the table.
+              </p>
+
+              <!-- Table with stripped rows -->
+              <table class="table datatable">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Actions</th>
+
+                  </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($products as $product): ?>
+                    <tr>
+                        <th scope="row"><?=$product['id']?></th>
+                        <td>
+                            <?=$product['nom']?>
+                        </td>
+                        <td><?=$product['description']?></td>
+                        <td><?=$product['prix']?></td>
+                        <td>
+                            <a href="#" class="btn btn-primary">Edit</a>
+                            <a href="#" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+              </table>
+              <!-- End Table with stripped rows -->
+
+              
+              
+              <!-- add product btn -->
+                <div class="row">
+                    <div class="col-lg-12 mt-2">
+                    <a href="#" class="btn btn-primary">Add Product</a>
+                </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section> 
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>STE CHATEUR FRERES</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      <!-- All the links in the footer should remain intact. -->
+      <!-- You can delete the links only if you purchased the pro version. -->
+      <!-- Licensing information: https://bootstrapmade.com/license/ -->
+      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+    
+    </div>
+  </footer><!-- End Footer --><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="../public/assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="../public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../public/assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="../public/assets/vendor/echarts/echarts.min.js"></script>
+  <script src="../public/assets/vendor/quill/quill.min.js"></script>
+  <script src="../public/assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="../public/assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="../public/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="../public/assets/js/main.js"></script>
+  <script>
+    let topBanner = document.getElementsByClassName("datatable-top")[0];
+    topBanner.style.display = "none";
+
+    let bottomBanner = document.getElementsByClassName("datatable-bottom")[0];
+    bottomBanner.style.display = "none";
+
+  </script>
+
+</body>
+
+</html>
